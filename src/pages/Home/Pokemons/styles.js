f13dc0f96b8a29341242c750bdf9dee723b10e9f
brenopinna <brenopinna1008@gmail.com>
2022-09-08 19:26:44 -0300

    responsive

--- src/pages/Home/Pokemons/styles.js
+++ src/pages/Home/Pokemons/styles.js
@@ -3,5 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
    display: grid;
    grid-template-columns: repeat(3, 1fr);
+   justify-content: center;
    gap: 5px 10px;
+
+   @media screen and (max-width: 768px){
+      grid-template-columns: repeat(2, 0.4fr);
+   }
+
+   @media screen and (max-width: 600px){
+      grid-template-columns: 0.7fr;
+   }
+
+   @media screen and (max-width: 400px){
+      grid-template-columns: 1fr;
+   }
 `;
